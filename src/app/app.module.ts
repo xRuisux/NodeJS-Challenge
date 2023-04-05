a2c6665b921f91a251fe614b6ef0e6b0d3dc99b1
LuizLima <luiz.c.brito1@gmail.com>
2023-04-05 10:28:35 -0200

    refactor(folder + configModule): adjusment on folders + add configModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { ImportHistoryModule } from './import-history/import-history.module';
+import app  from '../config/app'
+import database from '../config/database'
+import { MongooseConfigService } from './database/moongoose-config.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://admin:IaLhmQ8nBHbPA3JN@clusterproductstest.s3vppb9.mongodb.net/?retryWrites=true&w=majority'), ProductsModule, ImportHistoryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [app, database]
+    }),
+    MongooseModule.forRootAsync({useClass: MongooseConfigService}),
+     ProductsModule,
+      ImportHistoryModule
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
